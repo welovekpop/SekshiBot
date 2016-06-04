c32e719466f830f09b270a347a5929753f87b5a8
René Kooi <renee@kooi.me>
2016-06-04 13:23:41 +0200

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mongo: 'mongodb://localhost:27017/sekshi',
+  adapters: [
+    ['slack', { token: process.env.SLACK_TOKEN }],
+    ['uwave', {
+      api: 'https://welovekpop.club/v1',
+      socket: 'wss://welovekpop.club',
+      token: process.env.UWAVE_TOKEN
+    }],
+    ['plugdj', {
+      email: process.env.PLUGDJ_EMAIL,
+      password: process.env.PLUGDJ_PASSWORD,
+      room: 'loves-kpop'
+    }]
+  ],
+  plugins: [
+    ['chat-log', { enable: true }],
+    ['user-log', { enable: true }],
+    ['emotes', { enable: true }],
+    ['greetings', { enable: true }],
+    ['karma', { enable: true }],
+    ['triggers', { enable: true }],
+    ['waitlist-raffle', { enable: true }],
+    ['welovekpop-redirect', { enable: true }]
+  ]
+}
